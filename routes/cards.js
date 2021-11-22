bab8841a406018118e83d9b818265490e0f3528c
codetributor <okaclint@gmail.com>
2021-11-22 11:44:31 -1000

    new routes

--- routes/cards.js
+++ routes/cards.js
@@ -4,10 +4,31 @@ const router = express.Router();
 const {data} = require('../data/flashCardData.json');
 const {cards} = data;
 
+
+
 router.get('/:id', function(req, res) {
-    res.locals.prompt = cards[req.params.id].question;
-    res.locals.hint = cards[req.params.id].hint;
-    res.render('card');
+    const {side} = req.query;
+    const { id } = req.params;
+
+if(!side) {
+    return res.redirect(`/cards/${id}?side=question`)
+}
+
+    const text = cards[id][side];
+    const {hint} = cards[id];
+    
+    const name = req.cookies.username;
+    const templateData = { side, text, hint, side, id, name}
+    res.render('card', templateData);
 });
+
+router.get('/', function(req, res) {
+
+    let id = Math.floor(Math.random() * 5);
+
+    return res.redirect(`/cards/${id}`)
+
+});
+
 
 module.exports = router;
